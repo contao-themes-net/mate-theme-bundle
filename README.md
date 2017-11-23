37b3fe61db51b1c14440ebe1bb0c2271fe61fd71
Mathias Arzberger <develop@pdir.de>
2017-11-23 23:46:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# contao-themes-net/mate-theme-bundle
+mate - exhilarating contao theme for contao cms
+
+license
+
+pdir / contao theme licence -> see LICENSE file for more informations
+
+## compatible
+compatible with Contao 4.4.x
+
+## structure
+
+./src/ContaoThemesNet/MateThemeBundle/Resources
+    config = symlink to web (Images, JS, CSS etc.)
+    public = Symfony config (services etc.)
+    views  = Templates for Twig
+    contao = contao stuff (config, dca etc.)
+
---
+++ src/ContaoThemesNet/MateThemeBundle/ContaoManager/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * mate theme for Contao Open Source CMS
+ *
+ * Copyright (C) 2017 pdir / digital agentur <develop@pdir.de>
+ *
+ * @package    contao-themes-net/mate-theme-bundle
+ * @link       https://github.com/contao-themes-net/mate-theme-bundle
+ * @license    pdir / contao theme licence
+ * @author     Mathias Arzberger <develop@pdir.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ContaoThemesNet\MateThemeBundle;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Mathias Arzberger <develop@pdir.de>
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MateThemeBundle::class)
+                ->setLoadAfter([MateThemeBundle::class])
+        ];
+    }
+}
