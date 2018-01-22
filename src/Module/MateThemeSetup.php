89775056e173ce90dcc585bf752d45d6f188080f
Mathias Arzberger <develop@pdir.de>
2018-01-22 17:12:33 +0100

    add MateThemeSetup module

---
+++ src/Module/MateThemeSetup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ContaoThemesNet\MateThemeBundle\Module;
+
+class MateThemeSetup extends \BackendModule
+{
+    protected $strTemplate = 'be_mateTheme_setup';
+
+    /**
+     * Generate the module
+     */
+    protected function compile()
+    {
+
+    }
+}
