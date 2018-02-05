9cbfe31b9919f441c10148d6c62c9e10ba71cc1a
Philipp Seibt <seibt@pdir.de>
2018-02-05 16:10:09 +0100

    update: sql file, load scss files, images and theme setup function

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -14,8 +14,8 @@ class MateThemeSetup extends \BackendModule
         switch (\Input::get('act')) {
             case 'syncFolder':
                 $path = TL_ROOT . '/web/bundles/matetheme';
-                if(!file_exists("files/mate")) {
-                    new \Folder("files/mate");
+                if(!file_exists("files/mate1")) {
+                    new \Folder("files/mate1");
                 }
                 $this->getFiles($path);
                 $this->Template->message = true;
@@ -28,19 +28,24 @@ class MateThemeSetup extends \BackendModule
         foreach (scan($path) as $dir) {
             if(!is_dir($path."/".$dir)) {
                 $pos = strpos($path,"matetheme");
-                $filesFolder = "files/mate".str_replace("matetheme","",substr($path,$pos))."/".$dir;
-                if(!file_exists(TL_ROOT."/".$filesFolder)) {
-                    $objFile = new \File("web/bundles/".substr($path,$pos)."/".$dir, true);
-                    $objFile->copyTo($filesFolder);
+                $filesFolder = "files/mate1".str_replace("matetheme","",substr($path,$pos))."/".$dir;
+
+                if($dir != "_mate_variables.scss" && $dir != "backend.css" && $dir != "mate.scss" && $dir != "materialize.scss") {
+                    if(!file_exists(TL_ROOT."/".$filesFolder)) {
+                        $objFile = new \File("web/bundles/".substr($path,$pos)."/".$dir, true);
+                        $objFile->copyTo($filesFolder);
+                    }
                 }
             } else {
                 $folder = $path."/".$dir;
                 $pos = strpos($path,"matetheme");
-                $filesFolder = "files/mate".str_replace("matetheme","",substr($path,$pos))."/".$dir;
-                if(!file_exists($filesFolder)) {
-                    new \Folder($filesFolder);
+                $filesFolder = "files/mate1".str_replace("matetheme","",substr($path,$pos))."/".$dir;
+                if($dir != "fonts" && $dir != "js" && $dir != "components") {
+                    if(!file_exists($filesFolder)) {
+                        new \Folder($filesFolder);
+                    }
+                    $this->getFiles($folder);
                 }
-                $this->getFiles($folder);
             }
         }
     }
