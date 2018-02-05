4cc789b3b44eeb871386dc186219cc600a65068f
Philipp Seibt <seibt@pdir.de>
2018-02-05 17:09:10 +0100

    update mate theme setup

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -16,8 +16,8 @@ class MateThemeSetup extends \BackendModule
         switch (\Input::get('act')) {
             case 'syncFolder':
                 $path = TL_ROOT . '/web/bundles/matetheme';
-                if(!file_exists("files/mate1")) {
-                    new \Folder("files/mate1");
+                if(!file_exists("files/mate")) {
+                    new \Folder("files/mate");
                 }
                 $this->getFiles($path);
                 $this->Template->message = true;
@@ -30,7 +30,7 @@ class MateThemeSetup extends \BackendModule
         foreach (scan($path) as $dir) {
             if(!is_dir($path."/".$dir)) {
                 $pos = strpos($path,"matetheme");
-                $filesFolder = "files/mate1".str_replace("matetheme","",substr($path,$pos))."/".$dir;
+                $filesFolder = "files/mate".str_replace("matetheme","",substr($path,$pos))."/".$dir;
 
                 if($dir != "_mate_variables.scss" && $dir != "backend.css" && $dir != "mate.scss" && $dir != "materialize.scss") {
                     if(!file_exists(TL_ROOT."/".$filesFolder)) {
@@ -41,7 +41,7 @@ class MateThemeSetup extends \BackendModule
             } else {
                 $folder = $path."/".$dir;
                 $pos = strpos($path,"matetheme");
-                $filesFolder = "files/mate1".str_replace("matetheme","",substr($path,$pos))."/".$dir;
+                $filesFolder = "files/mate".str_replace("matetheme","",substr($path,$pos))."/".$dir;
                 if($dir != "fonts" && $dir != "js" && $dir != "components") {
                     if(!file_exists($filesFolder)) {
                         new \Folder($filesFolder);
