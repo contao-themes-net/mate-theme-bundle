d0fd573574601e3d75daf83a68c7637110b57cfb
Philipp Seibt <seibt@pdir.de>
2018-04-23 15:12:38 +0200

    update composer.json

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.0.31';
+    const VERSION = '1.0.32';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
