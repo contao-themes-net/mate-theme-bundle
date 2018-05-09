085cf13390d0660f9d671d1f404e32e7c25d37a5
Philipp Seibt <seibt@pdir.de>
2018-05-09 10:23:00 +0200

    update sql for contao 4.4 and load material icons selfhosting

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.0.33';
+    const VERSION = '1.0.34';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
