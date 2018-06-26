56cbdc823375e84aa9a158551974f67bf1e4cbbb
seibtph <seibt@pdir.de>
2018-06-26 14:25:02 +0200

    update version

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.0.36';
+    const VERSION = '1.0.37';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
