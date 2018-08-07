d7386f6ae1554e9be49a698f1e04de0f2ab43bd7
seibtph <seibt@pdir.de>
2018-08-07 14:36:52 +0200

    set headerimage to 100% width

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.2.1';
+    const VERSION = '1.2.2';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
