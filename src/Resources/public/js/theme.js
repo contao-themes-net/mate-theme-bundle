07a027310c9db0d1eef2972878952a3f4cf188f0
Philipp Seibt <seibt@pdir.de>
2018-10-23 14:12:07 +0200

    add image element to content slider

--- src/Resources/public/js/theme.js
+++ src/Resources/public/js/theme.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){ 
-	
+$(document).ready(function(){
+
 	/* ===================== *
 	 *   Formulare		 *
 	 * ===================== */
 	$('select').material_select();
-	
+
 	$inputDatepicker = $('.datepicker').pickadate({
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		container: 'body'
 	});
 	$inputDatepicker.pickadate('picker');
-	
+
 	$inputDatepicker = $('.timepicker').pickatime({
 	    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
 	    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
@@ -28,20 +28,20 @@ $(document).ready(function(){
 	    ampmclickable: true, // make AM PM clickable
 	    aftershow: function(){}, //Function for after opening timepicker
 	    container: 'body'
-	});	
+	});
 	$inputDatepicker.pickatime('picker');
-	
+
 	//$(".clockpicker").appendTo("body");
 	//$(".picker").appendTo("body");
-	
+
 	/* ===================== *
 	 *   Content Slider		 *
 	 * ===================== */
-	$( ".slides .ce_text" ).each(function( index ) {
+	$( ".slides .ce_text, .slides .ce_image" ).each(function( index ) {
 		var classList = $(this).attr('class');
 		$(this).replaceWith("<li class='" + classList + "'>" + $(this).html() + "</li>");
 	});
-	
+
 	/* ===================== *
 	 *   Mate Slider		 *
 	 * ===================== */
@@ -49,35 +49,35 @@ $(document).ready(function(){
 		height: 460,
 	    indicators: true,
 	    interval: 12000
-	}); 
-	
+	});
+
 	$(".slider.mod_newslist.smaller").slider({
 		height: 250,
 	    indicators: true,
 	    interval: 12000
 	});
 
-	
+
 	$( ".slider.mod_newslist .next" ).click(function() {
 	  $(".slider").slider("next");
 	});
-	
+
 	$( ".slider.mod_newslist .prev" ).click(function() {
 	  $(".slider").slider("prev");
 	});
-	
+
 	$( ".slider.mod_newslist .sliderImage" ).each(function( index ) {
 	  var headline = $(this).find("h2").text();
 	  var subheadline = $(this).find(".subheadline").text();
 	  var i = index;
-	  
+
 	  $( ".slider.mod_newslist .indicator-item" ).each(function( index ) {
 	  	if(i == index) {
 	  		$(this).append("<span class='inner'><span class='subheadline'>" + subheadline + "</span><span class='headline'>" + headline + "</span></span>");
 	  	}
 	  });
 	});
-	
+
 	/* ===================== *
 	*   Suche				 *
 	* ====================== */
@@ -90,21 +90,21 @@ $(document).ready(function(){
 			$('body').append('<div class="overlay active"></div>');
 		}
 	});
-	
-	$(document).click(function(event) { 
+
+	$(document).click(function(event) {
 	    if($(event.target).closest('.overlay').length) {
 	        $('.search-box').removeClass('active');
 	        $('div').remove('.overlay');
-	    }        
+	    }
 	});
-	
+
 	$(window).scroll(function () {
 		if($('.mod_mateNavbar').hasClass('headroom--unpinned')) {
 			$('.search-box').removeClass('active');
 			$('div').remove('.overlay');
 		}
 	});
-	
+
 	/* ===================== *
 	*   Navigation			 *
 	* ====================== */
@@ -129,7 +129,7 @@ $(document).ready(function(){
     }
 
 	$(".button-collapse").sideNav({});
-    
+
     $(".desktop-menu a.dropdown-button").dropdown({
       inDuration: 300,
       outDuration: 225,
@@ -138,7 +138,7 @@ $(document).ready(function(){
       alignment: "left",
       constrainWidth: false
     });
-    
+
     $(".mobile-menu a.dropdown-button").dropdown({
       inDuration: 300,
       outDuration: 225,
@@ -146,22 +146,22 @@ $(document).ready(function(){
       belowOrigin: true,
       alignment: "left"
     });
-	
+
 	/* ===================== *
 	*   Toplink  			 *
-	* ====================== */	
+	* ====================== */
 	$(window).scroll(function () {
 		scrollPos = $(document).scrollTop();
-		
+
 		$("footer .toplink").addClass("active");
     	if(scrollPos <= 500) {
     		$("footer .toplink").removeClass("active");
     	}
 	});
-	
+
 	$(document).on('click', 'footer .toplink', function(event){
 	    event.preventDefault();
-	
+
 		$("html, body").animate({ scrollTop: 0 }, 1000);
 	});
 
