f0361b4ea20e96e979fd6bbaf417770dd0bf2925
Philipp Seibt <seibt@pdir.de>
2019-03-13 14:54:55 +0100

    customizing bootstrap grid templates

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.7.0';
+    const VERSION = '1.7.1';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
