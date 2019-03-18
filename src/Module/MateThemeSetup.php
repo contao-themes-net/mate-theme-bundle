f87277327ee151f86ef1c3b550df27253ecb1b24
Philipp Seibt <seibt@pdir.de>
2019-03-18 11:53:07 +0100

    add font-display swap for roboto font

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.7.2';
+    const VERSION = '1.7.3';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
