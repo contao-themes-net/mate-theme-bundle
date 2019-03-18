a53ab9fd0f480206e2db273a7880e1af03e1456e
Philipp Seibt <seibt@pdir.de>
2019-03-18 15:42:02 +0100

    css fix for pagination

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '1.7.3';
+    const VERSION = '1.7.4';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
