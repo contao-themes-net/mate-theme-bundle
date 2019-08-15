9ef0aa915522276932e45fdea825e4fcc4e51d3c
Philipp Seibt <seibt@pdir.de>
2019-08-15 17:35:16 +0200

    update sql files, css and theme setup template

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.2.0';
+    const VERSION = '2.2.1';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
