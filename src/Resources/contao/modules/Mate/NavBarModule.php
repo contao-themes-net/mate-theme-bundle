be5c1ff695c28f900f4d9c7ac383dcbb08ede47a
Philipp Seibt <seibt@pdir.de>
2019-08-19 10:18:20 +0200

    add sql file for contao 4.8, fix navbar bug and css

--- src/Resources/contao/modules/Mate/NavBarModule.php
+++ src/Resources/contao/modules/Mate/NavBarModule.php
@@ -107,6 +107,7 @@ class NavBarModule extends \Module
             $this->mateRootTpl = 'nav_mate_left';
             $this->mateMobileTpl = 'nav_mate_mobile';
             $this->mateDropdownTpl = '';
+            $this->mateDropdownTplLvl3 = '';
         }
         if ($this->mateNavbarType == 'type5')
         {
