b2a0b40151f9c4068ba1e0b584adc833c4aab233
Philipp Seibt <seibt@pdir.de>
2019-10-25 10:22:12 +0200

    add sql files for the minimal version of contao

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.2.9';
+    const VERSION = '2.2.10';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
