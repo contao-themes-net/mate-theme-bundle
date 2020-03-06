5396cce3e02b53b2081371de8d2a7639296f7ec9
Philipp Seibt <seibt@pdir.de>
2020-03-06 14:40:43 +0100

    add breadcrumb

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.5.1';
+    const VERSION = '2.6.0';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
