ef27e62ffaf01ce54d338498de8fcb6dc428f6c6
seibt <seibt@pdir.de>
2020-03-06 15:04:52 +0100

    update version

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.6.0';
+    const VERSION = '2.7.0';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
