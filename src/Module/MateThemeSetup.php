03c60cef16bd197256b0dbf1cb3f8758e9075407
Philipp Seibt <seibt@pdir.de>
2020-07-15 15:13:20 +0200

    css fixes

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.10.2';
+    const VERSION = '2.10.3';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
