aaa7158b2d7e25aaa3f53db4b506ba3a9e5fac5c
Philipp Seibt <seibt@pdir.de>
2020-08-18 10:45:52 +0200

    fix css bug with mobile navigation

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.11.4';
+    const VERSION = '2.11.5';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
