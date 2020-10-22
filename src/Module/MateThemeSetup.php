3b44d15cb7019661efda9710438e368642af2b34
Philipp Seibt <seibt@pdir.de>
2020-10-22 14:32:17 +0200

    add new parallax template with container width

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.12.1';
+    const VERSION = '2.12.2';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
