331b2e964199fa405b35a1dca2f3ea42700d15f3
Philipp Seibt <seibt@pdir.de>
2020-11-06 17:17:38 +0100

    css fix for text list with image

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.12.2';
+    const VERSION = '2.12.3';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
