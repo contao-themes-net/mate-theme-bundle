6e15103b2e2739079e1a9c6e8da774f81c133ba3
Philipp Seibt <seibt@pdir.de>
2020-12-16 17:48:33 +0100

    update teaserbox

--- src/Resources/contao/elements/Mate/TeaserBox.php
+++ src/Resources/contao/elements/Mate/TeaserBox.php
@@ -62,5 +62,25 @@ class TeaserBox extends \ContentElement
         $this->Template->metaImg = unserialize(\FilesModel::findByUuid($this->singleSRC)->meta);
         $this->Template->pageText = $this->mateTeaserBox_pageText;
         $this->Template->subheadline = $this->mateTeaserbox_subHeadline;
+
+        // add an image
+        if ($this->addImage && $this->singleSRC != '')
+        {
+            $objModel = \FilesModel::findByUuid($this->singleSRC);
+            if ($objModel !== null && is_file(\System::getContainer()->getParameter('kernel.project_dir') . '/' . $objModel->path))
+            {
+                $this->singleSRC = $objModel->path;
+                $this->addImageToTemplate($this->Template, $this->arrData, null, null, $objModel);
+            }
+        }
+
+        // overwrite link target
+        $this->Template->target = '';
+        $this->Template->rel = '';
+        if ($this->target)
+        {
+            $this->Template->target = ' target="_blank"';
+            $this->Template->rel = ' rel="noreferrer noopener"';
+        }
     }
 }
