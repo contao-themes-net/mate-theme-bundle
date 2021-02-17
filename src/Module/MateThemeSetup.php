74bc645b75b5fdc425a0b63d79efe7fd0d7538e8
Philipp Seibt <seibt@pdir.de>
2021-02-17 10:28:55 +0100

    update material icons version

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.13.0';
+    const VERSION = '2.13.1';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
