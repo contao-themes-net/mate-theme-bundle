2fb12d7249b8434577b29e8aeb207e204bd29def
Philipp Seibt <seibt@pdir.de>
2021-03-15 10:32:56 +0100

    responsive tables and remove jquery conflict

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.13.1';
+    const VERSION = '2.14.0';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
