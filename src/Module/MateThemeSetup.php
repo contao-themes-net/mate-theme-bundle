f2b647dd59040c5da72fa5e251a25404e449340a
Philipp Seibt <seibt@pdir.de>
2021-08-25 11:39:55 +0200

    add sql files for contao 4.12

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.15.3';
+    const VERSION = '2.16.0';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
