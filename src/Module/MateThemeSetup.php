b6f2b31f1c5956843cebc434db6bc1dcc9c09b57
Philipp Seibt <seibt@pdir.de>
2021-08-26 11:49:22 +0200

    css fix for subnav

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.16.0';
+    const VERSION = '2.16.1';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
