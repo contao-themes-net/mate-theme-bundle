8aaa6d1eaf9801b0c8dbf921903fb4d661be904e
Philipp Seibt <seibt@pdir.de>
2021-11-10 17:41:07 +0100

    update changelog and version

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -4,7 +4,7 @@ namespace ContaoThemesNet\MateThemeBundle\Module;
 
 class MateThemeSetup extends \BackendModule
 {
-    const VERSION = '2.17.3';
+    const VERSION = '2.17.4';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
