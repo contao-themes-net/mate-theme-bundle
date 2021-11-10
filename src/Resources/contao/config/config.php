4900f8bdacff0338a7d0e408b9f0d5a95b5046dd
Philipp Seibt <seibt@pdir.de>
2021-11-10 17:39:42 +0100

    remove php warning in php8

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,7 +12,7 @@ $GLOBALS['FE_MOD']['mateTheme']['mateNavbar'] = 'ContaoThemesNet\\MateThemeBundl
 $GLOBALS['FE_MOD']['mateTheme']['mateModal'] = 'ContaoThemesNet\\MateThemeBundle\\Mate\\ModalModule';
 
 if($GLOBALS['FE_MOD']['news']['newslist']) {
-    if ( $GLOBALS['BE_MOD']['pdir']['socialFeed'] ) {
+    if ( !empty($GLOBALS['BE_MOD']['pdir']['socialFeed']) ) {
         $GLOBALS['FE_MOD']['news']['newslist'] = 'ContaoThemesNet\\MateThemeBundle\\Module\\ModuleNewsListMateSocialFeed';
     } else {
         $GLOBALS['FE_MOD']['news']['newslist'] = 'ContaoThemesNet\\MateThemeBundle\\Module\\ModuleNewsListMate';
