54ce76b3608b82b510c94a8f4a83ae91faf77fb0
Mathias Arzberger <develop@pdir.de>
2022-03-09 18:25:56 +0100

    fix extension file: remove unused variables

--- src/DependencyInjection/MateThemeBundleExtension.php
+++ src/DependencyInjection/MateThemeBundleExtension.php
@@ -2,34 +2,23 @@
 
 namespace ContaoThemesNet\MateThemeBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class MateThemeBundleExtension extends Extension
+class MateThemeExtension extends Extension
 {
-    /**
-     * Usage in Controller
-     *
-     * $custom = $this->container->getParameter( 'mate_theme.assets.custom_scss' );
-     *
-     */
-
     /**
      * {@inheritdoc}
      */
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function load(array $mergedConfig, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(__DIR__ . '/../Resources/config')
         );
 
-        $loader->load('services.yaml');
-
-        $container->setParameter( 'mate_theme.assets.custom_scss', $mergedConfig[ 'assets.custom_scss' ]);
-        $container->setParameter( 'mate_theme.assets.custom_variables_scss', $mergedConfig[ 'assets.custom_variables_scss' ]);
+        $loader->load('services.yml');
     }
 }
