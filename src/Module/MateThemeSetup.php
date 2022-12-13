670d0807c0df764cf6f9dbe537e8e2cf6d1f63e2
Philipp Seibt <seibt@pdir.de>
2022-12-13 14:16:16 +0100

    Fix scss path and remove no longer used files

--- src/Module/MateThemeSetup.php
+++ src/Module/MateThemeSetup.php
@@ -10,7 +10,7 @@ use ContaoThemesNet\MateThemeBundle\ThemeUtils;
 
 class MateThemeSetup extends BackendModule
 {
-    const VERSION = '2.19.11';
+    const VERSION = '2.19.12';
 
     protected $strTemplate = 'be_mateTheme_setup';
 
