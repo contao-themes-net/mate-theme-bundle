7d41a4881beee246a94db2a54d2f7bb49b9944d5
Philipp Seibt <seibt@pdir.de>
2023-01-02 17:15:44 +0100

    fix theme.js

--- public/js/theme.js
+++ public/js/theme.js
@@ -144,7 +144,7 @@ jQuery(document).ready(function ($) {
         $(".slider-text-below-image li").each(function () {
             var headline = $(this).find("> h2, > h3, > h4, > h5, > h6").html();
             var index = $(this).index() + 1;
-            var slider = $($(this).closest('.slider-text-below-image'));
+            var slider = $(this).closest('.slider-text-below-image');
             setTimeout(function () {
                 slider.find(".indicators li:nth-of-type(" + index + ")").html(headline);
             }, 100);
