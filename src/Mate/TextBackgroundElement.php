d191f1eb951c6fc6c7a05a912edccaa77cf0b4d9
Mathias Arzberger <develop@pdir.de>
2023-03-09 16:22:05 +0100

    Update TextBackgroundElement.php

--- src/Mate/TextBackgroundElement.php
+++ src/Mate/TextBackgroundElement.php
@@ -67,8 +67,8 @@ class TextBackgroundElement extends ContentElement
     protected function compile(): void
     {
         // background image
-        $uuid = FilesModel::findByUuid($this->mate_background_image);
-        $this->Template->backgroundImage = $uuid->path;
+        $file = FilesModel::findByUuid($this->mate_background_image);
+        $this->Template->backgroundImage = null !== $file ? $file->path: null;
 
         // image
         if ($this->addImage && '' !== $this->singleSRC) {
