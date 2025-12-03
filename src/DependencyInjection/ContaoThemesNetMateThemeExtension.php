7f5f50a43c4168d357bc556824dba58654ed5684
Philipp Seibt <seibt@pdir.de>
2025-12-03 16:14:01 +0100

    ecs fix

--- src/DependencyInjection/ContaoThemesNetMateThemeExtension.php
+++ src/DependencyInjection/ContaoThemesNetMateThemeExtension.php
@@ -38,8 +38,9 @@ class ContaoThemesNetMateThemeExtension extends Extension
 
         $loader->load('services.yaml');
 
-        if ($container->hasParameter('kernel.bundles')
-            && array_key_exists('ContaoNewsBundle', $container->getParameter('kernel.bundles'))
+        if (
+            $container->hasParameter('kernel.bundles')
+            && \array_key_exists('ContaoNewsBundle', $container->getParameter('kernel.bundles'))
         ) {
             $loader->load('services_news.yaml');
         }
